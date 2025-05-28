e5f02e9f53463132ad702d4357165d93d283c58a
Sienna <sienna.harvey@connect.qut.edu.au>
2025-05-28 10:02:36 +1000

    fix

--- Assignment/Assets/Sienna Harvey/DoorTest.cs
+++ Assignment/Assets/Sienna Harvey/DoorTest.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class DoorTest : MonoBehaviour
 {
-    Animation animation;
+    public Animation animation;
 
     // Use this for initialization
     void Start()
     {
-        animation = GetComponent<Animation>();
+       
     }
     // Update is called once per frame
     void Update()
