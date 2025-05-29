830c247a46c2d74bda5ffd1951d9fb86abb16f0e
ryan <ryanbramley06@gmail.com>
2025-05-29 12:37:10 +1000

    Added Ryan Bramley Version

---
+++ Assignment/Assets/Ryan Bramley/Scripts/Pickup.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        // Debug checks
+        if (gameManager == null)
+        {
+            Debug.LogError("GameManager not found! Make sure GameObject has 'GameManager' tag.");
+        }
+        else
+        {
+            Debug.Log("GameManager connected successfully!");
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider otherObject)
+    {
+        Debug.Log("Something entered trigger: " + otherObject.name + " with tag: " + otherObject.tag);
+
+        if (otherObject.transform.tag == "Player")
+        {
+            Debug.Log("Player detected!");
+            gameManager.currentPickups += 1;
+            Debug.Log("Pickups now: " + gameManager.currentPickups);
+            Destroy(this.gameObject);
+        }
+    }
+}
